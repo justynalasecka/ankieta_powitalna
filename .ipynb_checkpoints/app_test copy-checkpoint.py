ecfe7f4413578d16aec52d269d28ef73c29d3231
Justyna <jmk.konopka@gmail.com>
2024-12-19 19:19:24 +0100

    ciemne tło

---
+++ .ipynb_checkpoints/app_test copy-checkpoint.py
@@ -0,0 +1,117 @@
+import json
+import streamlit as st
+import pandas as pd # type: ignore
+from pycaret.clustering import load_model, predict_model  # type: ignore
+import plotly.express as px  # type: ignore
+
+MODEL_NAME = 'model_v2'
+
+DATA = 'welcome_survey_simple_v2.csv'
+
+CLUSTER_NAMES_AND_DESCRIPTIONS = 'welcome_survey_cluster_names_and_descriptions_v2.json'
+
+@st.cache_data
+def get_model():
+    return load_model(MODEL_NAME)
+
+@st.cache_data
+def get_cluster_names_and_descriptions():
+    with open(CLUSTER_NAMES_AND_DESCRIPTIONS, "r", encoding='utf-8') as f:
+        return json.loads(f.read())
+    
+@st.cache_data # ma wczytać plik z danymi tylko raz, bez odświeżania
+def get_all_participants():
+    model = get_model()
+    all_df = pd.read_csv(DATA, sep=';')
+    df_with_clusters = predict_model(model, data=all_df)
+
+    return df_with_clusters
+
+st.title('Znajdź znajomych')
+with st.sidebar:
+    st.header("Powiedz nam coś o sobie")
+    st.markdown("Pomożemy Ci znaleźć osoby, które mają podobne zainteresowania. Zostaniesz przypisany do jednej z 4 grup.")
+    age = st.selectbox("Wiek", ['<18', '18-24', '25-34', '35-44', '45-54', '55-64','>=65', 'unknown'])
+    edu_level = st.selectbox("Wykształcenie", ['Podstawowe', 'Średnie', 'Wyższe'])
+    fav_animals = st.selectbox("Ulubione zwierzęta", ['Brak ulubionych', 'Psy', 'Koty', 'Inne', 'Koty i Psy'])
+    fav_place = st.selectbox("Ulubione miejsce", ['Nad wodą', 'W lesie', 'W górach', 'Inne'])
+    gender = st.radio("Płeć", ['Mężczyzna', 'Kobieta'])
+
+    person_df = pd.DataFrame([
+        {
+            'age': age,
+            'edu_level': edu_level,
+            'fav_animals': fav_animals,
+            'fav_place': fav_place,
+            'gender': gender
+        }
+    ])
+
+model = get_model()
+all_df = get_all_participants()
+cluster_names_and_descriptions = get_cluster_names_and_descriptions()
+
+predicted_cluster_id = predict_model(model, data=person_df)["Cluster"].values[0]
+predicted_cluster_data = cluster_names_and_descriptions[predicted_cluster_id]
+
+st.header(f"Najbliżej Ci do grupy {predicted_cluster_data['name']}")
+st.markdown(predicted_cluster_data['description'])
+same_cluster_df = all_df[all_df["Cluster"] == predicted_cluster_id]
+st.metric("Liczba twoich znajomych", len(same_cluster_df))
+
+st.header("Osoby z grupy")
+
+fig1 = px.histogram(same_cluster_df, x="gender", color_discrete_sequence=px.colors.sequential.Mint)
+fig1.update_layout(
+    title="Rozkład płci w grupie",
+    xaxis_title="Płeć",
+    yaxis_title="Liczba osób",
+)
+
+fig2 = px.histogram(same_cluster_df, x="edu_level", color_discrete_sequence=px.colors.sequential.Peach)
+fig2.update_layout(
+    title="Rozkład wykształcenia w grupie",
+    xaxis_title="Wykształcenie",
+    yaxis_title="Liczba osób",
+)
+
+fig3 = px.histogram(same_cluster_df.sort_values("age"), x="age", color_discrete_sequence=px.colors.sequential.Inferno_r)
+fig3.update_layout(
+    title="Rozkład wieku w grupie",
+    xaxis_title="Wiek",
+    yaxis_title="Liczba osób",
+)
+
+fig4 = px.histogram(same_cluster_df, x="fav_animals", color_discrete_sequence=px.colors.sequential.Peach_r)
+fig4.update_layout(
+    title="Rozkład ulubionych zwierząt w grupie",
+    xaxis_title="Ulubione zwierzęta",
+    yaxis_title="Liczba osób",
+    xaxis_tickangle=45,
+)
+
+fig5 = px.histogram(same_cluster_df, x="fav_place", color_discrete_sequence=px.colors.sequential.Electric_r)
+fig5.update_layout(
+    title="Rozkład ulubionych miejsc w grupie",
+    xaxis_title="Ulubione miejsce",
+    yaxis_title="Liczba osób",
+)
+
+# Utwórzmy obiekt zakładek
+tab0, tab1 = st.tabs(["Dane ogólne","Preferencje"])
+
+with tab0:
+    w1, w2 = st.columns(2)
+    with w1:
+        st.plotly_chart(fig1)
+    with w2:
+        st.plotly_chart(fig2)
+        
+    st.plotly_chart(fig3)
+
+with tab1:
+    w4, w5 = st.columns(2)
+    with w4:
+        st.plotly_chart(fig4)
+    with w5:
+        st.plotly_chart(fig5)
